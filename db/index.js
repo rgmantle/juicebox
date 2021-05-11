81a0750285dc0912da3c4d6a8e27413ea13b7c6f
rgmantle <graig.mantle@gmail.com>
2021-05-10 21:16:35 -0700

    day 1 upload api builds

--- db/index.js
+++ db/index.js
@@ -62,6 +62,19 @@ async function getAllUsers() {
   }
 }
 
+async function getAllTags() {
+  try {
+    const { rows } = await client.query(`
+      SELECT * 
+      FROM tags;
+    `);
+
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
 async function getUserById(userId) {
   try {
     const { rows: [ user ] } = await client.query(`
@@ -311,5 +324,6 @@ module.exports = {
   getAllPosts,
   getPostById,
   getPostsByUser,
-  getPostsByTagName
+  getPostsByTagName,
+  getAllTags
 }
---
+++ index.js
@@ -0,0 +1,47 @@
+//CRUD Web Server
+//...........HTTP........SQL
+// Create --> POST ---> INSERT
+// Retrieve --> GET ---> SELECT
+// Update --> PUT ---> UPDATE
+// Delete --> DELETE ---> DELETE
+
+
+const PORT = 3000;
+const { response } = require('express');
+const express = require('express');
+const server = express();
+
+const bodyParser = require('body-parser');
+server.use(bodyParser.json());
+
+const morgan = require('morgan');
+server.use(morgan('dev'));
+
+const { client } = require('./db');
+client.connect();
+
+const apiRouter = require('./api');
+server.use('/api', apiRouter);
+
+server.use((req, res, next) => {
+    console.log("<____Body Logger START____>");
+    console.log(req.body);
+    console.log("<_____Body Logger END_____>");
+  
+    next();
+  });
+
+server.use('/api', (request, response, next) => {
+    console.log("A request was made to /api (this is the middleware).");
+    next();
+})
+
+
+server.get('/api', (request, response, next) => {
+    console.log("A get request was made to /api");
+    response.send({message: "success"});
+})
+
+server.listen(PORT, () => {
+  console.log('The server is up on port', PORT)
+});
